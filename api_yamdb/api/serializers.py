a90567cf9c017db14690766762ecf941b6a88186
Тамерлан Салим <st3ls@yandex.ru>
2022-12-01 18:18:40 +0300

    эндпоинты title, category, genre

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from reviews.models import *
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    pass
