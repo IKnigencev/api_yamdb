51a1b042dadda7f3ebe13fd4e0c7fa4a02891696
Progqwer <ilyatsygancov@gmail.com>
2022-12-02 16:36:06 +0300

    reviews, comments, rating

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import ReviewViewSet, CommentViewSet
+
+from .views import CommentViewSet, ReviewViewSet
 
 router_v1 = routers.DefaultRouter()
 router_v1.register(
